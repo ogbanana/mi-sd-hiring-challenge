1095a4509888be714ddac266ca1f43d56f647d48
Amy Liao <a.liao.uey@gmail.com>
2021-05-05 01:00:36 -0400

    Implemented a form that takes a zipcode to gather weather results from the 2 differen APIs.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,57 @@
-import { convertDate } from "./utils";
+import { getGeolocationEndpoint, getDay } from './utils';
 
-console.log("hello world");
+const form = document.getElementById('zipcode-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getGeolocationEndpoint();
+});
+
+const app = document.getElementById('app');
+
+export function createWeatherElement (daily, city, regionCode) {
+  const weatherContainer = document.createElement('div');
+  weatherContainer.className = 'weather-container';
+  app.appendChild(weatherContainer);
+
+  const weatherHeader = document.createElement('h2');
+  weatherHeader.className = 'weather-header';
+  weatherHeader.innerText = `WEATHER FORECAST FOR ${city.toUpperCase()}, ${regionCode}`;
+  weatherContainer.appendChild(weatherHeader);
+
+  const forecastContainer = document.createElement('div');
+  forecastContainer.className = 'forecast-container';
+  weatherContainer.appendChild(forecastContainer);
+
+  daily.data.forEach((day, index) => {
+    const dayContainer = document.createElement('div');
+    dayContainer.className = 'day-container';
+    forecastContainer.appendChild(dayContainer);
+
+    const weekDay = document.createElement('h3');
+    weekDay.className = 'weekday';
+    weekDay.innerText = index === 0 ? 'Today' : getDay(index);
+    dayContainer.appendChild(weekDay);
+
+    const tempContainer = document.createElement('div');
+    tempContainer.className = 'temp-container';
+    dayContainer.appendChild(tempContainer);
+
+    const forecastIcon = document.createElement('img');
+    forecastIcon.className = 'forecast-icon';
+    forecastIcon.src = `/img/${day.icon}.png`;
+
+    const highTemp = document.createElement('p');
+    highTemp.className = 'high-temp';
+    highTemp.innerText = `${Math.floor(day.temperatureHigh)} F`;
+
+    const lowTemp = document.createElement('p');
+    lowTemp.className = 'low-temp';
+    lowTemp.innerText = `${Math.floor(day.temperatureMin)} F`;
+
+    const weatherDescription = document.createElement('p');
+    weatherDescription.className = 'weather-description';
+    weatherDescription.innerText = day.summary;
+
+    tempContainer.innerHTML = forecastIcon.outerHTML + weatherDescription.outerHTML + highTemp.outerHTML + lowTemp.outerHTML;
+  });
+}
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,76 @@
  *
  * @param {number} time - Unix time in seconds returns it in miliseconds
  */
+import {createWeatherElement} from './app.js';
+const geoAPI = 'https://se-weather-api.herokuapp.com/api/v1/geo';
+const forecastAPI = 'https://se-weather-api.herokuapp.com/api/v1/forecast';
+
 export function convertDate(time) {
   return time * 1000;
 }
+
+export function getDate() {
+  const date = new Date();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear()
+  return month + day + year;
+}
+
+export function getDay (idx) {
+  const days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const date = new Date();
+  let day = date.getDay();
+  if ((day + idx) > 6) day = day - 7;
+
+  return days[day + idx];
+}
+
+async function fetchGeolocationAPI (input) {
+  try {
+    const response = await fetch(`${geoAPI}?zip_code=${input}`);
+    return response.json();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function fetchForecastAPI(lat, long) {
+  const date = getDate();
+  try {
+    const forecast = await fetch(`${forecastAPI}?latitude=${lat}&longitude=${long}&date=${date}`);
+    return forecast.json();
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function getGeolocationEndpoint() {
+  const input = document.getElementById('zipcode-input').value;
+  const {
+    city,
+    latitude,
+    longitude,
+    regionCode
+  } = await fetchGeolocationAPI(input);
+
+  const { daily } = await fetchForecastAPI(latitude, longitude);
+  createWeatherElement(daily, city, regionCode);
+}
+
+// async function success(position) {
+//     const latitude  = position.coords.latitude;
+//     const longitude = position.coords.longitude;
+//     console.log(latitude, longitude);
+//     const { daily } = await fetchForecastAPI(latitude, longitude);
+//     console.log(daily)
+//   }
+//
+// export function getMylocation() {
+//   if (!navigator.geolocation) {
+//     alert('Unfortunately, we cannot find you with this browser.');
+//   } else {
+//     navigator.geolocation.getCurrentPosition(success);
+//   }
+// }
